e772a51af584e5b50e18a6a01a0385e00bebd12d
archana <archana@archana.com>
2014-05-27 22:04:17 -0700

    All tests pass. AssertJ added

--- src/main/java/TaxCalculator.java
+++ src/main/java/TaxCalculator.java
@@ -8,13 +8,34 @@ public class TaxCalculator {
 
     public static final double INC_21450 = 21450.0;
     public static final double RATE_015 = 0.15;
+    private static final double RATE_028 = 0.28;
+    private static final double RATE_031 = 0.31;
+    public static final int INC_51900 = 51900;
+    public static final double INC_35800 = 35800.0;
+    private static final double INC_86500 = 86500.0;
 
     public double calculateTax(boolean single, double income) {
         double tax = 0.0;
         if (single) {
             if (income <= INC_21450) {
                 tax = income * RATE_015;
+            } else if (income <= INC_51900) {
+                tax = income * RATE_028;
+            } else if (income > INC_51900) {
+                tax = income * RATE_031;
             }
+        } else
+        //if ( not single)
+        {
+            if (income <= INC_35800) {
+                tax = income * RATE_015;
+            } else if (income <= INC_86500) {
+                tax = income * RATE_028;
+            } else if (income > INC_86500) {
+                tax = income * RATE_031;
+            }
+
+
         }
 
 
@@ -22,4 +43,14 @@ public class TaxCalculator {
     }
 
 
+    // public double calculateTax(boolean married, double income) {
+    // double tax = 0.0;
+
+    //  return tax;
 }
+
+
+
+
+
+
