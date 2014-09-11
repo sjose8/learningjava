8015ab5b87c2647cc2287996146b106306cfdf07
amit <amitsk1974@gmail.com>
2014-09-10 21:36:12 -0700

    Scala stuff deleted.

--- src/test/scala/com/bitbucket/learningjava/AcceptanceSpec.scala
+++
@@ -1,7 +0,0 @@
-package com.bitbucket.learningjava
-
-
-import org.scalatest._
-
-abstract class AcceptanceSpec extends FeatureSpec with Matchers with
-OptionValues with Inside with Inspectors with GivenWhenThen
